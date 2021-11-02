c7860e53ba93256e395bd62951981d0a39213fd5
ParthivKrithik5 <93578040+ParthivKrithik5@users.noreply.github.com>
2021-11-02 14:19:47 +0530

    Update pipeline.py

--- pipeline.py
+++ pipeline.py
@@ -1,15 +1,15 @@
 import os
 list1 = [8,9,3,6,1,10]
 list1.reverse()
-print("The Reversed list is ",list1)
+print(f"The Reversed list is {list1}")
 
 list2 = [91,67,120,34,76,54,78,87,56,64,345]
 list2.sort()
-print("The sorted list is",list2)
+print(f"The sorted list is {list2}")
 
 list3 = []
 list3 = list1.copy()
-print("list3",list3)
+print(f"list3:{list3}")
 
 indexvalue = list2[2:6]
-print("The Index values are ",indexvalue)
+print(f"The Index values are : {indexvalue}")
